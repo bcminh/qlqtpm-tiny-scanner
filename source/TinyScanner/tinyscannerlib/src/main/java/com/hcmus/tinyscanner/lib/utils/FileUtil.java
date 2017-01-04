1808cc6f732b823f3d8014363a9357d083b4f174
Danny Minh <danny.minh0893@gmail.com>
2017-01-05 00:21:56 +0700

    add utils, appveyor.yml

--- source/TinyScanner/tinyscannerlib/src/main/java/com/hcmus/tinyscanner/lib/utils/FileUtil.java
+++ source/TinyScanner/tinyscannerlib/src/main/java/com/hcmus/tinyscanner/lib/utils/FileUtil.java
@@ -1,8 +1,89 @@
 package com.hcmus.tinyscanner.lib.utils;
 
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created by danny on 1/2/2017.
  */
 
 public class FileUtil {
+    public static File BASED_APP_DIR = Environment.getExternalStorageDirectory();
+    public static String createDocumentDir(int idDoc) {
+        Calendar rightNow = Calendar.getInstance();
+        File mediaStorageDir = new File(BASED_APP_DIR, idDoc + "_" + rightNow.getTimeInMillis());
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        return mediaStorageDir.getAbsolutePath();
+    }
+
+    public static String savePage(String dirDoc, int idPage, Bitmap imageBitmap) {
+        String pageName = idPage + "_" + Calendar.getInstance().getTimeInMillis() + ".png";
+        String pagePath = new File(dirDoc, pageName).getAbsolutePath();
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(pagePath);
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (Exception e) {
+            pagePath = null;
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return pagePath;
+    }
+
+    public static boolean deletePage(String dirPage) {
+        return new File(dirPage).delete();
+    }
+
+    public  static void deleteDocument(String dirDoc) {
+        deleteRecursive(new File(dirDoc));
+    }
+
+    private static void deleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            for (File child : fileOrDirectory.listFiles()) {
+                deleteRecursive(child);
+            }
+        }
+        fileOrDirectory.delete();
+    }
+
+    public static void exportPDF(String dirDoc, String dirName, List<String> imagePaths)  {
+        Document document=new Document();
+        try {
+            PdfWriter.getInstance(document,new FileOutputStream("YourPDFHere.pdf"));
+            document.open();
+            for (String imagePath : imagePaths) {
+                Image image = Image.getInstance (imagePath);
+                document.add(new Paragraph(dirName));
+                document.add(image);
+            }
+            document.close();
+        } catch (DocumentException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- source/TinyScanner/tinyscannerlib/src/main/java/com/hcmus/tinyscanner/lib/utils/ImageUtil.java
+++ source/TinyScanner/tinyscannerlib/src/main/java/com/hcmus/tinyscanner/lib/utils/ImageUtil.java
@@ -1,8 +1,33 @@
 package com.hcmus.tinyscanner.lib.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
 /**
  * Created by danny on 1/2/2017.
  */
 
 public class ImageUtil {
+    public static Bitmap rotateBitmap(Bitmap original, int angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        return Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), matrix, true);
+    }
+
+    public static Mat bitmapToMat(Bitmap bitmap) {
+        Mat mat = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8U, new Scalar(4));
+        Bitmap bitmap32 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bitmap32, mat);
+        return mat;
+    }
+
+    public static Bitmap matToBitmap(Mat mat) {
+        Bitmap bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat, bitmap);
+        return bitmap;
+    }
 }
